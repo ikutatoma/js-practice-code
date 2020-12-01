39d0f89399e97a939179009267f70c60cc44a497
ikutatoma <58389158+ikutatoma@users.noreply.github.com>
2020-12-01 21:16:16 +0900

    a

--- 4-29.js
+++ 4-29.js
@@ -37,6 +37,26 @@ function readUserInput(question) {
   // メイン処理
   (async function main() {
     console.log("整数を加算します。")
+    var total = 0;
+    outer: {
+      for (var i = 0; i <= 10; i++) {
+        console.log("第" + i + "グループ");
 
+        inner: {
+          for (var j = 0; j < 5; j++) {
+            var a = await readUserInput("整数:");
+            var t = parseInt(a);
+            if (t == 99999) {
+              break outer;
+            } else if (t == 88888) {
+              break inner;
+            }
+            total += t;
+          }
+        }
+
+      }
+    }
+    console.log("合計は" + total + "です。");
   })();
 })();
---
+++ 4-30.js
@@ -0,0 +1,41 @@
+const { Console } = require('console');
+
+function readUserInput(question) {
+  const readline = require('readline').createInterface({
+    input: process.stdin,
+    output: process.stdout
+  });
+
+  return new Promise((resolve, reject) => {
+    readline.question(question, (answer) => {
+      resolve(answer);
+      readline.close();
+    });
+  });
+}
+// メイン処理
+(async function main() {
+  var max_no = 6;
+  var left_no = max_no;
+  var max = 99
+  var min = 0
+  const no = Math.floor(Math.random() * (max + 1 - min)) + min;//0-100 の整数の乱数
+  console.log("数当てゲーム");
+  console.log("10~99の数の当ててください。");
+  do {
+    var n = await readUserInput("あと" + (left_no--) + "回。いくつかな:");
+    var x = parseInt(n);
+    if (x > no) {
+      console.log("もっと小さな数だよ！")
+    }
+    else if (x < no) {
+      console.log("もっと大きな数だよ！")
+    }
+  } while (x != no && left_no > 0);
+  if (x == no) {
+    console.log((max_no - left_no) + "回で当たりましたね。");
+  }
+  else {
+    console.log("残念。正解は" + no + "でした。");
+  }
+})();
