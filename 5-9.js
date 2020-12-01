48c43601a7f31ac87ef512f7d6653bba6b833947
ikutatoma <58389158+ikutatoma@users.noreply.github.com>
2020-12-02 08:00:39 +0900

    a

--- 5-9.js
+++ 5-9.js
@@ -13,5 +13,12 @@ function readUserInput(question) {
 }
 // メイン処理
 (async function main() {
+  console.log("  float               xの2乗  ");
+  console.log("----------------------------------");
+  var x = parseFloat(0.0);
+  for (var i = 0; i <= 1000; i++, x += 0.001) {
+    var x = parseFloat(i /1000);
+    console.log(x + "   " + (x * x));
+  }
 
 })();
