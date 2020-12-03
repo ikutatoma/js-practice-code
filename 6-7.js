b616a42d57ef48c347b48b139c07e97de4b0b176
ikutatoma <58389158+ikutatoma@users.noreply.github.com>
2020-12-03 22:20:45 +0900

    a

--- 6-7.js
+++ 6-7.js
@@ -13,12 +13,29 @@ function readUserInput(question) {
 }
 // メイン処理
 (async function main() {
-  const a = await readUserInput("点数:");
+  const a = await readUserInput("人数:");
   const ninzu = parseFloat(a);
   console.log("点数を入力せよ。");
   var sum = 0;
   var tensu = new Array();
-  for (var i = 0; i < ninzu;i++){
-
+  for (var i = 0; i < ninzu; i++) {
+    var c = await readUserInput((i + 1) + "番の点数");
+    var score = parseInt(c);
+    tensu[i] = score;
+    sum += tensu[i];
   }
+  var max = tensu[0];
+  var min = tensu[0];
+  for (var i = 1; i < ninzu; i++) {
+    if (tensu[i] > max) {
+      max = tensu[i];
+    }
+    if (tensu[i] < min) {
+      min = tensu[i];
+    }
+  }
+  console.log("合計点は" + sum + "点です。");
+  console.log("平均点は" + (sum / ninzu) + "点です。");
+  console.log("最高点は" + max + "点です。");
+  console.log("最低点は" + min + "点です。");
 })();
