562d5437a796f7fa8776a6adffc179581b1230c1
ikutatoma <58389158+ikutatoma@users.noreply.github.com>
2020-12-04 11:22:52 +0900

    a

--- 6-8.js
+++ 6-8.js
@@ -13,5 +13,28 @@ function readUserInput(question) {
 }
 // メイン処理
 (async function main() {
+  const b = await readUserInput("要素数:");
+  const n = parseInt(b);
+  var a = new Array();
+
+  for (var j = 0; j < n; j++) {
+    var g = await readUserInput("a[" + j + "] = ");
+    var score = parseInt(g);
+    a[j] = score;
+  }
+
+  var z = await readUserInput("探す数値:");
+  var key = parseInt(z);
+  var i = 0;
+  for (i = 0; i < n; i++) {
+    if (a[i] == key) {
+      break;
+    }
+  }
+  if (i < n) {
+    console.log("それはa[" + i + "]にあります。");
+  } else {
+    console.log("それはありません。");
+  }
 
 })();
