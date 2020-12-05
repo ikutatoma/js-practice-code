dddca2aa9ce74728a339fa76f8db6fe6ee4e0c81
ikutatoma <58389158+ikutatoma@users.noreply.github.com>
2020-12-05 21:09:51 +0900

    a

--- 6-18.js
+++ 6-18.js
@@ -13,5 +13,25 @@ function readUserInput(question) {
 }
 // メイン処理
 (async function main() {
+    const a_row = 4;//行
+  const a_column = 3;//列
+
+  var a_arr = new Array(a_row);
+  for (var i = 0; i < a_row; i++) {
+    a_arr[i] = new Array(a_column).fill(0);
+  };
+  const b_row = 3;//行
+  const b_column = 4;//列
+
+  var b_arr = new Array(b_row);
+  for (var i = 0; i < b_row; i++) {
+    b_arr[i] = new Array(b_column).fill(0);
+  };
+
+  console.log("行列aの要素の値を入力せよ。");
+  for (var i = 0; i < a_row;i++){
+    var a_ele = await readUserInput("a);
+  }
+  console.log("行列bの要素の値を入力せよ。");
 
 })();
