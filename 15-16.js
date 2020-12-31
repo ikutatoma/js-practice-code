ac23360904b7401751150eddb1842473f3a302af
ikutatoma <58389158+ikutatoma@users.noreply.github.com>
2020-12-31 17:07:40 +0900

    a

--- 15-16.js
+++ 15-16.js
@@ -13,23 +13,12 @@ function readUserInput(question) {
 // メイン処理
 (async function main() {
   class Calendar {
-    constructor(year, month, date) {
+    constructor(year, month) {
       this.year = year;
       this.month = month;
-      this.date = date;
-
-      if (this.year == void 0 || this.month == void 0 || this.date == void 0) {
-        this.year = (this.year == void 0) ? 1 : this.year;
-        this.month = (this.month == void 0) ? 1 : this.month;
-        this.date = (this.date == void 0) ? 1 : this.date;
-      }
     }
     dayOfweek() {
-      if (this.month == 1 || this.month == 2) {
-        this.year--;
-        this.month += 12;
-      }
-      var date = new Date(year, month);
+      var date = new Date(this.year, this.month);
       return date.getDay();
       /* return parseInt(year + year / 4 - year / 100 + year / 400 + (13 * month + 8) / 5 + day) % 7;
        */
@@ -40,15 +29,15 @@ function readUserInput(question) {
     }
 
     monthDays() {
-      if (this.month-- != 2) {
+      if (this.month != 1) {
         return mday[this.month];
       }
-      return mday[this.month] + (isLeap(this.year) ? 1 : 0);
+      return mday[this.month] + (this.isLeap(this.year) ? 1 : 0);
     }
-
 
     putCalendar() {
       var wd = this.dayOfweek();
+      // console.log(wd);
       var mdays = this.monthDays();
       console.log("日 月 火 水 木 金 土");
       console.log("-------------------");
@@ -59,31 +48,34 @@ function readUserInput(question) {
         var twoI = ('00' + i).slice(-2);
         process.stdout.write(String(twoI) + " ");
         wd++;
-        if (wd % 7 == 0) {
+
+        if (wd % 7 == 0 && i == mdays) {} else if (wd % 7 == 0) {
           console.log("\n");
         }
       }
     }
   }
-   mday = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+  mday = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
   year = parseInt(process.argv[2]);
-  month =parseInt(process.argv[3]);
-  if (process.argv.length == 0) {
-    today = new Calendar(new Date());
+  month = parseInt(process.argv[3]) - 1;
+
+  if (isNaN(year) == true && isNaN(month) == true) {
+    today = new Date();
     year = today.getFullYear();
-    month = today.getMonth() + 1;
+    month = today.getMonth();
   }
-  console.log(year);
+
   var cale = new Calendar(year, month);
-  console.log(cale)
-  if (process.argv.length == 0 || process.argv.length >= 2) {
-    console.log(year + "年" + month + "月のカレンダー");
+
+  if (isNaN(year) == false && isNaN(month) == false) {
+    console.log(year + "年" + (month + 1) + "月のカレンダー");
     cale.putCalendar();
   } else {
     console.log(year + "年のカレンダー");
-    for (month = 1; month <= 12; month++) {
-      console.log(month + "月");
-      putCalendar(year, month);
+    for (month = 0; month <= 11; month++) {
+      console.log(`\n\n${month + 1}月のカレンダー`);
+      cale = new Calendar(year, month)
+      cale.putCalendar();
     }
   }
 })();
